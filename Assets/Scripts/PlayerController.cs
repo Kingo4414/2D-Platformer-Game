824ec27d24e33133a336fe581fc065ca80d28226
Kingo4414 <97949996+Kingo4414@users.noreply.github.com>
2022-02-01 20:41:13 +0530

    Chapter 2 - Assignment - Crouch
    Chapter 2 - Assignment - Crouch
    Challenge 1 - (Do it yourself with all you are able to learn by applying all mini Assignments )
    
    [Task 1] - Make crouch animation and on pressing Cntrl key on keyboard player crouches and we also resize the collider.
    
    [Task 2] - Accept the vertical in the same way as we accepted the horizontal input and when vertical > 0 then play jump animation.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,14 +14,78 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
+       
+
+        Crouch();
+
+        Jump();
+        Speed();
+
+
+       
+
+    }
+
+    public void Crouch() {
+        bool crouch = Input.GetKey("left ctrl");
+
+        if (crouch == true)
+        {
+            gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.53f, 1.29f);
+            gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.59f);
+
+            animator.SetBool("Crouch", true);
+            //   Debug.Log("right ctrl keydown bool pressed...");
+        }
+        else if (crouch == false)
+        {
+            gameObject.GetComponent<BoxCollider2D>().size = new Vector2(0.62f, 2.01f);
+            gameObject.GetComponent<BoxCollider2D>().offset = new Vector2(0.02f, 0.97f);
+
+            animator.SetBool("Crouch", false);
+           
+        }
+
+    }
+    public void Jump() {
+        animator.SetBool("Jump", false);
+        float jump = Input.GetAxisRaw("Vertical");
+        
+        Vector3 scale = transform.localScale;
+        if (jump > 0)
+        {
+            
+            animator.SetBool("Jump", true);
+           scale.x = -1f * Mathf.Abs(scale.x);
+            Debug.Log("jump >0 pressed..." +jump);
+            
+        }
+        else if (jump == 0)
+        { 
+            animator.SetBool("Jump", false);
+           scale.x = Mathf.Abs(scale.x);
+            Debug.Log("jump <0 pressed..." + jump);
+        }
+       // transform.localScale = scale;
+        
+    }
+    public void Speed() {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
-        if (speed<0) {
+        if (speed < 0)
+        {
             scale.x = -1f * Mathf.Abs(scale.x);
-        }else if (speed > 0){
+        }
+        else if (speed > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
         }
-        transform.localScale =scale;
+        transform.localScale = scale;
+
     }
-}
+        
+        
+        
+        }
