fac6263b311fbda9d0189cd294bd0b0028b9d29b
Kingo4414 <97949996+Kingo4414@users.noreply.github.com>
2022-02-03 21:11:42 +0530

    PlayerController script changes
    added Rigidbody2D modified Jump script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,30 +7,44 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public float speed;
+    public float jump;
+    private Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    private void Awake()
+    {
+        Debug.Log("Player Controller Awake");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
     // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-
-        MoveCharacter(horizontal);
-        PlayerMovemntAnimation(horizontal);
+        float vertical = Input.GetAxisRaw("Jump");
+       
+        MoveCharacter(horizontal, vertical);
+        PlayerMovemntAnimation(horizontal, vertical);
 
 
        
 
     }
 
-    private void MoveCharacter(float horizontal)
-    {
+    private void MoveCharacter(float horizontal,float vertical)
+    {  //MoveCharacter horizontally
         Vector3 position = transform.position;
         position.x +=  horizontal * speed * Time.deltaTime;
         transform.position = position;
+
+        //MoveCharacter vertically
+        if (vertical > 0) {
+            rb2d.AddForce(new Vector2(0f,jump),ForceMode2D.Force);
+        }
+
     }
 
     private void Crouch() {
@@ -55,7 +69,7 @@ public class PlayerController : MonoBehaviour
 
     }
   
-    private void PlayerMovemntAnimation(float horizontal) {
+    private void PlayerMovemntAnimation(float horizontal, float vertical) {
        
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -71,23 +85,20 @@ public class PlayerController : MonoBehaviour
 
         Crouch();
 
+        //Jump   
         animator.SetBool("Jump", false);
-        float jump = Input.GetAxisRaw("Vertical");
 
-        
-        if (jump > 0)
+        if (vertical > 0)
         {
 
             animator.SetBool("Jump", true);
-            scale.x = -1f * Mathf.Abs(scale.x);
-            Debug.Log("jump >0 pressed..." + jump);
+            Debug.Log("jump >0 pressed..." + vertical);
 
         }
-        else if (jump < 0)
+        else if (vertical < 0)
         {
             animator.SetBool("Jump", false);
-            scale.x = Mathf.Abs(scale.x);
-            Debug.Log("jump <0 pressed..." + jump);
+            Debug.Log("jump <0 pressed..." + vertical);
         }
         // transform.localScale = scale;
 
